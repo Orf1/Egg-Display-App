838f2bf8237f6a7444dd95e9d5444c89dd2ad0b0
Orf1 <orfeas126@gmail.com>
2022-05-31 00:55:24 -0700

    Initial Commit

---
+++ Egg Display/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Egg Display
+//
+//  Created by Orfeas Magoulas on 5/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
